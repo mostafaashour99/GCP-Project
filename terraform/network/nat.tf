7199ad757a2b29797dd0fb9a7ee908b3f6a3dcff
mostafaashour99 <mostafa01284248878@gmail.com>
2022-10-30 22:46:11 +0200

    final

--- terraform/network/nat.tf
+++ terraform/network/nat.tf
@@ -22,10 +22,10 @@ resource "google_compute_router_nat" "nat" {
   region = google_compute_subnetwork.management-subnetwork.region
 
   nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
 
-  # subnetwork {
-  #   name = google_compute_subnetwork.management-subnetwork.id
-  #   source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-  # }
+  subnetwork {
+    name = google_compute_subnetwork.management-subnetwork.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
 }
